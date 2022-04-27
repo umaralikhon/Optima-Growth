9adb1dd7af1a84393f42ac85913b208d8597c951
Umaralikhon <umaralikhon@gmail.com>
2022-04-27 23:32:01 +0500

    Docker compose

--- src/main/java/com/optimagrowth/license/model/License.java
+++ src/main/java/com/optimagrowth/license/model/License.java
@@ -8,7 +8,7 @@ import org.springframework.hateoas.RepresentationModel;
 @Getter
 @Setter
 @ToString
-public class License extends RepresentationModel<License> {
+public class License extends RepresentationModel<License > {
 
     private int id;
     private String licenseId;
